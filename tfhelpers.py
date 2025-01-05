ea78066be69a22989c606edcd37874bfcaf9a1fd
Vidur Parkash <vidur.parkash@seagate.com>
2025-01-05 13:43:37 -0600

    creare importable tensorflow helper functions

---
+++ tfhelpers.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def TFPlotLossCurves( TrainingHistory, Epochs ):
+    """
+    Plots Training Curves returned from Tensorflow Fit() function
+    
+    Args:
+        TrainingHistory: Training History Object returned from fit()
+        
+        Epochs: Training Epochs
+        
+    Returns:
+        none
+    """
+
+    epochs = np.linspace( 1, Epochs, 1 )
+    if 'val_loss' in TrainingHistory.history:
+        ValDataPresent = True
+    else:
+        ValDataPresent = False
+    
+    plt.subplot(1,2,1)
+    if ValDataPresent:
+
+        plt.plot( epochs, TrainingHistory.history['loss'], label = 'loss', color = 'red' )
+        plt.plot( epochs, TrainingHistory.history['val_loss'], label = 'val_loss', color = 'blue' )
+        plt.axis( False )
+        plt.legend( )
+        plt.show( )
+    else:
+        plt.plot( epochs, TrainingHistory.history['loss'], label = 'loss', color = 'red' )
+        plt.axis( False )
+        plt.legend( )
+        plt.show( )
+
+    plt.subplot(1,2,2)
+    if ValDataPresent:
+
+        plt.plot( epochs, TrainingHistory.history['accuracy'], label = 'accuracy', color = 'red' )
+        plt.plot( epochs, TrainingHistory.history['val_accuracy'], label = 'val_accuracy', color = 'blue' )
+        plt.axis( False )
+        plt.legend( )
+        plt.show( )
+    else:
+        plt.plot( epochs, TrainingHistory.history['accuracy'], label = 'accuracy', color = 'red' )
+        plt.axis( False )
+        plt.legend( )
+        plt.show( )
